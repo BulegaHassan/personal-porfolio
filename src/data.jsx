0d50099da381702396f790b5c094179b4a703a77
Hassan Bulega <hassanbulega@gmail.com>
2023-09-01 23:15:40 +0300

    add more skills data

--- src/data.jsx
+++ src/data.jsx
@@ -1,6 +1,17 @@
 import { nanoid } from "nanoid";
-import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
-
+// import { FaHtml5, FaJs, FaReact } from "react-icons/fa";
+import {
+  SiAdobeaftereffects,
+  SiAdobeaudition,
+  SiAdobefonts,
+  SiAdobeillustrator,
+  SiAdobeindesign,
+  SiAdobelightroom,
+  SiAdobephotoshop,
+  SiAdobexd,
+  SiCanva,
+  SiPhotopea,
+} from "react-icons/si";
 export const links = [
   { id: nanoid(), href: "#home", text: "home" },
   { id: nanoid(), href: "#about", text: "about" },
@@ -13,21 +24,57 @@ export const links = [
 export const skills = [
   {
     id: nanoid(),
-    title: "HTML&CSS",
-    icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
+    title: "Adobexd",
+    icon: <SiAdobexd className='h-16 w-16 text-emerald-500' />,
     text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
   },
   {
     id: nanoid(),
-    title: "Javascript",
-    icon: <FaJs className='h-16 w-16 text-emerald-500' />,
+    title: "Canva",
+    icon: <SiCanva className='h-16 w-16 text-emerald-500' />,
     text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
   },
   {
     id: nanoid(),
-    title: "React",
-    icon: <FaReact className='h-16 w-16 text-emerald-500' />,
+    title: "Adobephotoshop",
+    icon: <SiAdobephotoshop className='h-16 w-16 text-emerald-500' />,
     text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+  {
+    id: nanoid(),
+    title: "Adobeindesign",
+    icon: <SiAdobeindesign className='h-16 w-16 text-emerald-500' />,
+    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+  },
+  {
+    id: nanoid(),
+    title: "Adobeillustrator",
+    icon: <SiAdobeillustrator className='h-16 w-16 text-emerald-500' />,
+    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+  },
+  {
+    id: nanoid(),
+    title: "Adobefonts",
+    icon: <SiAdobefonts className='h-16 w-16 text-emerald-500' />,
+    text: "Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.",
+  },
+  {
+    id: nanoid(),
+    title: "Adobeaudition",
+    icon: <SiAdobeaudition className='h-16 w-16 text-emerald-500' />,
+    text: "Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.",
+  },
+  {
+    id: nanoid(),
+    title: "Adobeaftereffects",
+    icon: <SiAdobeaftereffects className='h-16 w-16 text-emerald-500' />,
+    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
+  },
+  {
+    id: nanoid(),
+    title: "Photopea",
+    icon: <SiPhotopea className='h-16 w-16 text-emerald-500' />,
+    text: "Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality",
   },
 ];
 
