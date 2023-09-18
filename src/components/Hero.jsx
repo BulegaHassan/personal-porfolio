f697cc2a0d14aea1015ce0e11ce5c0190e23f61e
Hassan Bulega <hassanbulega@gmail.com>
2023-09-18 12:34:15 +0300

    change icons and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
 import heroImg from "../assets/hero_dev.svg";
-import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTwitter,FaStackOverflow, } from "react-icons/fa";
+import {BiLogoUpwork} from 'react-icons/bi'
 const Hero = () => {
   return (
-    <section role="region" className='bg-emerald-100 py-24 dark:bg-slate-800 dark:text-white dark:border-x-8 dark:border-emerald-100'>
+    <section
+      role='region'
+      className='bg-emerald-100 py-24 dark:bg-slate-800 dark:text-white dark:border-x-8 dark:border-emerald-100'
+    >
       <div className=' mx-auto max-w-7xl  px-8  grid  md:grid-cols-2 items-center gap-8'>
         <article>
-          <h1 role="heading" className='text-7xl font-bold tracking-wider'>
+          <h1 role='heading' className='text-7xl font-bold tracking-wider'>
             I'm Hassan Bulega
           </h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide dark:text-white'>
@@ -17,21 +21,33 @@ const Hero = () => {
           </p>
           <div className='flex gap-x-4 mt-4'>
             <a href='#'>
-              <FaGithubSquare
-                className='h-8 w-8 text-slate-500 hover:text-black duration-300'
+              <FaGithub
+                className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='github-icon'
               />
             </a>
             <a href='#'>
               <FaLinkedin
-                className='h-8 w-8 text-slate-500 hover:text-black duration-300'
+                className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='linkedin-icon'
               />
             </a>
             <a href='#'>
-              <FaTwitterSquare
-                className='h-8 w-8 text-slate-500 hover:text-black duration-300'
+              <FaTwitter
+                className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='twitter-icon'
+              />
+            </a>
+            <a href='#'>
+              <FaStackOverflow
+                className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
+                data-testid='stackoverflow-icon'
+              />
+            </a>
+            <a href='#'>
+              <BiLogoUpwork
+                className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
+                data-testid='upwork-icon'
               />
             </a>
           </div>
