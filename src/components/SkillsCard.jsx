14fac6a925712c465cd23898246f0839b5b1116d
Hassan Bulega <hassanbulega@gmail.com>
2023-10-10 14:09:05 +0300

    feat: group related skills and add category field

--- src/components/SkillsCard.jsx
+++ src/components/SkillsCard.jsx
@@ -1,9 +1,20 @@
-const SkillsCard = ({ icon, title, text }) => {
+const SkillsCard = ({ icon1, icon2, icon3, title, text, category }) => {
   return (
     <article>
-      <span className='h-16 w-16'>{icon}</span>
-      <h4 className='mt-6 font-bold'>{title}</h4>
-      <p className='mt-2 text-slate-500 dark:text-slate-300'>{text}</p>
+      <div className='flex justify-center'>
+        <span className='h-16 w-16 '>{icon1}</span>
+        <span className='h-16 w-16'>{icon2}</span>
+        <span className='h-16 w-16'>{icon3}</span>
+      </div>
+      <h4 className='mt-6 font-bold text-center'>{title}</h4>
+      <p className='mt-2 text-center text-slate-500 dark:text-slate-300'>
+        {text}
+      </p>
+      <div className='flex justify-center'>
+        <p className='mt-2 p-0.5 inline   w-auto rounded  bg-emerald-300 text-slate-500 dark:text-slate-700'>
+          {category}
+        </p>
+      </div>
     </article>
   );
 };
