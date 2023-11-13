d34bf0882064f3a7b0593a79b9ab4ed0fa4ed3ef
Hassan Bulega <hassanbulega@gmail.com>
2023-11-13 15:09:10 +0300

    styles: add shadow class

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import { skills } from "../data";
 const Skills = () => {
   return (
     <section
-      className='py-20 dark:bg-slate-800 dark:text-white dark:border-x-8 dark:border-emerald-400'
+      className='py-20 shadow-sm dark:bg-slate-800 dark:text-white dark:border-x-8 dark:border-emerald-400'
       id='skills'
     >
       <h2 className='text-center text-4xl font-medium tracking-wider capitalize underline decoration-emerald-300 dark:decoration-dotted'>
