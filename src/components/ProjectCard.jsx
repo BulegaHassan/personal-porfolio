9069f0d4a03648662176803136d2d44baa63a4d2
Hassan Bulega <hassanbulega@gmail.com>
2023-11-13 15:07:45 +0300

    add links to site and github

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
-// import { FaGithubSquare, FaTwitterSquare } from "react-icons/fa";
-// import { TbWorldWww } from "react-icons/tb";
-const ProjectCard = ({ img, title, text }) => {
+import { FaGithubSquare } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+const ProjectCard = ({ img, title, text,url,github }) => {
   return (
     <article
       className='bg-white rounded-lg shadow-md block hover:shadow-xl duration-300 hover:translate-x-1 hover:translate-y-1 dark:bg-slate-800'
@@ -12,13 +12,21 @@ const ProjectCard = ({ img, title, text }) => {
         alt={title}
         className='w-full object-cover rounded-t-lg h-64 '
       />
-      <div className='capitalize p-8'>
-        <h2 className='text-xl tracking-wide font-medium text-center '>
+      <div className=' p-8'>
+        <h2 className='capitalize text-xl tracking-wide font-medium  '>
           {title}
         </h2>
-        <p className='mt-4 text-slate-500 dark:text-slate-300 leading-loose text-center'>
+        <p className='mt-4 text-slate-500 dark:text-slate-300 leading-loose '>
           {text}
         </p>
+        <div className='mt-4 flex gap-x-4'>
+          <a href={url} target='_blank' rel="noreferrer">
+            <TbWorldWww className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+          </a>
+          <a href={github} target='_blank' rel="noreferrer">
+            <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+          </a>
+        </div>
       </div>
     </article>
   );
