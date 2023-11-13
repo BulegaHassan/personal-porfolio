4c093089ed427d797bb31d165511a83f4e81bc05
Hassan Bulega <hassanbulega@gmail.com>
2023-11-13 15:06:16 +0300

    fix links add real links to href

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
 import heroImg from "../assets/hero_dev.svg";
-import { FaGithub, FaLinkedin, FaTwitter,FaStackOverflow, } from "react-icons/fa";
-import {BiLogoUpwork} from "react-icons/bi";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaStackOverflow,
+} from "react-icons/fa";
+import { BiLogoUpwork } from "react-icons/bi";
 const Hero = () => {
   return (
     <section
@@ -13,38 +18,37 @@ const Hero = () => {
             I'm Hassan Bulega
           </h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide dark:text-white'>
-            Full Stack Engineer
+            Full Stack Developer
           </p>
           <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide dark:text-white'>
-            turning ideas into exciting and interactive reality to represent
-            your presence worldwide. be it business or portfolio.
+            Building fullstack web apps. responsive, awesome user interface and experience. Website,webapp, backend,databases? Youre are covered! 
           </p>
           <div className='flex gap-x-4 mt-4'>
-            <a href='#'>
+            <a href='https://github.com/BulegaHassan' target="_blank" rel="noreferrer">
               <FaGithub
                 className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='github-icon'
               />
             </a>
-            <a href='#'>
+            <a href='https://www.linkedin.com/in/hassan-bulega/' target="_blank" rel="noreferrer">
               <FaLinkedin
                 className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='linkedin-icon'
               />
             </a>
-            <a href='#'>
+            <a href='https://twitter.com/hassanbulega' target="_blank" rel="noreferrer">
               <FaTwitter
                 className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='twitter-icon'
               />
             </a>
-            <a href='#'>
+            <a href='https://stackoverflow.com/users/16943019/hassan-bulega' target="_blank" rel="noreferrer">
               <FaStackOverflow
                 className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='stackoverflow-icon'
               />
             </a>
-            <a href='#'>
+            <a href='https://www.upwork.com/freelancers/~011c8726b82c6191a9' target="_blank" rel="noreferrer">
               <BiLogoUpwork
                 className='h-8 w-8 text-emerald-600 hover:text-emerald-800 duration-300'
                 data-testid='upwork-icon'
