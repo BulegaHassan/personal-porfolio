4cb90af626504e1d4a5e8f4ab2302fae359a46fd
Hassan Bulega <hassanbulega@gmail.com>
2024-01-09 11:49:28 +0300

    feat: show partial or full content on click

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+const info = `
+  Experienced Full Stack Developer with 2+ years in MERN stack,
+            specializing in JavaScript and transitioning to TypeScript.
+            Proficient in front-end and back-end development, excelling in
+            crafting responsive web applications and RESTful APIs. Skilled in
+            HTML, CSS, React.js, Node.js, and Express.js. Strong testing
+            practices using Jest, Vitest, Supertest, and Cypress ensure code
+            quality. Expertise extends to SQL and NoSQL databases, with hands-on
+            experience in implementing automated CI/CD pipelines using GitHub
+            Actions and Azure Pipelines. Efficient deployment strategies using
+            Firebase, Flyctl, Render, and cPanel hosting. Committed to
+            continuous learning and problem-solving, I am passionate about
+            delivering high-quality solutions. A collaborative team player,
+            currently expanding skills in C#, Azure DevOps, Python and cutting-edge
+            technologies.
+`;
 const About = () => {
+  const [readMore, setReadMore] = useState(false);
+
   return (
     <section
       className='bg-white pt-20 dark:bg-slate-800 dark:text-white dark:border-x-8 dark:border-emerald-400'
@@ -15,20 +34,13 @@ const About = () => {
           </h2>
 
           <p className='text-slate-500 md:text-center dark:text-slate-300 mt-8 leading-loose'>
-            Experienced Full Stack Developer with 2+ years in MERN stack,
-            specializing in JavaScript and transitioning to TypeScript.
-            Proficient in front-end and back-end development, excelling in
-            crafting responsive web applications and RESTful APIs. Skilled in
-            HTML, CSS, React.js, Node.js, and Express.js. Strong testing
-            practices using Jest, Vitest, Supertest, and Cypress ensure code
-            quality. Expertise extends to SQL and NoSQL databases, with hands-on
-            experience in implementing automated CI/CD pipelines using GitHub
-            Actions and Azure Pipelines. Efficient deployment strategies using
-            Firebase, Flyctl, Render, and cPanel hosting. Committed to
-            continuous learning and problem-solving, I am passionate about
-            delivering high-quality solutions. A collaborative team player,
-            currently expanding skills in C#, Azure DevOps, Python and cutting-edge
-            technologies.
+            {readMore ? info : `${info.substring(0, 500)}...  `}
+            <button
+              className='text-emerald-700 text-lg'
+              onClick={() => setReadMore(!readMore)}
+            >
+              {readMore ? "show less" : "  read more"}
+            </button>
           </p>
         </article>
       </div>
